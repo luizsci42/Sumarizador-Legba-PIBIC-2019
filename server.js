71b01ca4f6521ae65e392733541bfc9ad5e9c775
Luiz Felipe Souza <luizsci42@gmail.com>
2019-09-11 22:47:38 -0300

    Teste para iniciar o ReactJS no Heroku

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 // Utiliza as rotas definidas em rotasConsulta, passando nossa instância do express como argumento
 require('./rotas/rotasConsulta')(app);
 
+// Set static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 // Este programa será executado na porta 5000. Acese localhost:3000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
